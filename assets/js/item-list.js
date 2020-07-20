8127ed2dd52ae4ee6d8302b80a32dd3e147c6df0
Céline Desurmont <cdesurmo@gmail.com>
2020-07-20 10:06:31 +0200

    Home page (item list) : item-list.js -> added the loop This way, it repeats the following protocol for each object (= teddy) sent by the API : create a wrapper for the new item card, add its classes, add the HTML structure of the item card inside of the wrapper, filled with the teddy information

--- assets/js/item-list.js
+++ assets/js/item-list.js
@@ -1,4 +1,4 @@
-// To add dynamically the inner HTML structure of the first item card and add the informations of the 1st teddy bear
+// To add dynamically a new item wrapper for each object (=each teddy) sent by the API, and add the HTML structure for the item card inside of the wrapper, filled with the information of the teddy
 
 document.addEventListener('DOMContentLoaded', function (event) {
 
@@ -7,40 +7,47 @@ document.addEventListener('DOMContentLoaded', function (event) {
     let askItemsInfo = new XMLHttpRequest();
 
     askItemsInfo.onreadystatechange = function () {
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            let response = JSON.parse(this.responseText);
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200)
+        {
+            let teddies = JSON.parse(this.responseText);
 
-            itemListContainer.innerHTML = " <div class=\"col col-lg-8 my-5 mx-lg-auto\">\n" +
-                "                            <div class=\"card bg-white shadow\">\n" +
-                "                                <div class=\"card-body\">\n" +
-                "                                    <div class=\"card-image item-image\">\n" +
-                "                                       <img src='" + response.imageUrl + "'>" +
-                "                                    </div>\n" +
-                "\n" +
-                "                                    <h3 class=\"card-title item-name\">\n" +
-                                                         response.name +
-                "                                    </h3>\n" +
-                "\n" +
-                "                                    <div class=\"card-text\">\n" +
-                "                                        <p class=\"item-description\">\n" +
-                                                            response.description +
-                "                                        </p>\n" +
-                "                                        <div class=\"item-price\">\n" +
-                "                                            Prix: <span class=\"item-price__number\">"+ response.price + " €</span>\n" +
-                "                                        </div>\n" +
-                "                                    </div>\n" +
-                "                                </div>\n" +
-                "                            </div>\n" +
-                "                        </div>"
+            for (let teddy of teddies)
+            {
+                const newItemWrapper = document.createElement('div');
+                itemListContainer.appendChild(newItemWrapper);
+                newItemWrapper.classList.add('col', 'col-lg-8', 'my-5', 'mx-lg-auto');
+
+                newItemWrapper.innerHTML =
+                    '                            <div class=\"card bg-white shadow\">' +
+                    '                                <div class=\"card-body\">' +
+                    '                                    <div class=\"card-image item-image\">' +
+                    '                                       <img src="' + teddy.imageUrl + '">' +
+                    '                                    </div>' +
+                    '                                       <h3 class=\"card-title item-name\">' +
+                                                                teddy.name +
+                    '                                       </h3>' +
+                    '                                    <div class=\"card-text\">' +
+                    '                                        <p class=\"item-description\">' +
+                                                                teddy.description +
+                    '                                        </p>' +
+                    '                                        <div class=\"item-price\">' +
+                    '                                            Prix: <span class=\"item-price__number\">' + teddy.price + ' €</span>' +
+                    '                                        </div>' +
+                    '                                    </div>' +
+                    '                                </div>' +
+                    '                            </div>' +
+                    '                        </div>';
+            }
         }
-        else {
-            // Todo : Handle error here
-            // console.log('ststus', this.status);
-            // console.log('readyState', this.readyState);
-        }
+        else
+            {
+                // Todo : Handle error here
+                // console.log('ststus', this.status);
+                // console.log('readyState', this.readyState);
+            }
     };
 
-    askItemsInfo.open('GET', 'http://localhost:3000/api/teddies/5be9c8541c9d440000665243');
+    askItemsInfo.open('GET', 'http://localhost:3000/api/teddies/');
     askItemsInfo.send();
 });
 
