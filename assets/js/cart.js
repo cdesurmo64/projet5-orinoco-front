56a802760c7a9c47f61c1f44c0f80449b4447d75
Céline Desurmont <cdesurmo@gmail.com>
2020-08-04 10:37:14 +0200

    Cart page : Added the "back to home page" button and the "empty the cart" button, a part of its CSS style and its JS functionality * Created emptyCart() (in cart.js) : - remove the cart from the localStorage - execute resetCartTable() (= new function in cart.js) -> remove the content from the cart table body - execute showAllItemsInCart() -> check if there is no cart in the localStorage as expected and show the Empty Cart Message - execute displayNumberArticlesCartIcon() -> check if there is no cart in the localStorage and reset tje number of article displayed in the cart icon to 0.

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -16,16 +16,66 @@ const showAllItemsInCart = function() {
             displayCartItemInformation(itemInCart);
             fullPrice += Number(itemInCart.price * itemInCart.quantity);
         }
-        orderFullPrice.textContent = fullPrice;
+        orderFullPrice.textContent = fullPrice.toString();
     }
 
     else {
         displayEmptyCartMessage();
     }
-};
+}
+
+
+;/**
+ * Remove all the content from the cart table body to reset it
+ * And reset the order full price to 0 €
+ */
+const resetCartTable = function () {
+
+        const cartTableContent = document.getElementById('cart-table-content');
+        const orderFullPrice = document.getElementById('order-full-price');
+
+        while (cartTableContent.firstChild) {
+            cartTableContent.removeChild(cartTableContent.firstChild);
+        }
+        orderFullPrice.textContent = '0';
+    };
 
 
 // To execute showAllItemsInCart() when DOM content is loaded (because it uses DOM elements)
 document.addEventListener('DOMContentLoaded', function (event) {
     showAllItemsInCart();
+});
+
+
+/**
+ * If there is a 'cart' in the localStorage :
+ * - Empty the cart by removing the 'cart' item from the localStorage.
+ * - Reset the cart table content by executing resetCartTable().
+ * - Execute showAllItemsInCart() that check if the localStorage is effectively empty
+ *   and display the empty cart message.
+ * - Execute displayNumberArticlesCartIcon() that check if the localStorage is effectively empty
+ *   and reset the number of article displayed in the cart icon to 0.
+ *
+ * The button which needs to be clicked to empty the cart is only visible when there is something in the cart
+ * so no need for an 'else' action.
+ */
+const emptyCart = function() {
+
+    const allItemsInCart = JSON.parse(localStorage.getItem('cart'));
+
+    if (allItemsInCart) {
+        localStorage.removeItem('cart');
+        resetCartTable();
+        showAllItemsInCart();
+        displayNumberArticlesCartIcon();
+    }
+};
+
+
+
+// To execute emptyCart() when the empty-cart-button element is clicked
+const emptyCartButton = document.getElementById('empty-cart-button');
+
+emptyCartButton.addEventListener('click', function () {
+    emptyCart();
 });
--- assets/js/display.js
+++ assets/js/display.js
@@ -146,8 +146,10 @@ const displayCustomisationMenu = function (product) {
 
 /**
  * Get the quantities of teddies added in the cart from the localStorage
- * Sum all these quantities to have the total number of teddies in the cart
- * and display it on the cart icon.
+ * If there is at least one teddy in the cart :
+ * sum all these quantities to have the total number of teddies in the cart.
+ * If there is none : the number of teddies in cart remains equal to 0
+ * Finally : display the nb of teddies in cart on the cart icon.
  */
 const displayNumberArticlesCartIcon = function () {
     const numberArticlesCartIcon = document.getElementById('cart-icon-nb-articles');
@@ -159,8 +161,8 @@ const displayNumberArticlesCartIcon = function () {
         {
             nbTeddiesInCart += Number(teddyInCart.quantity);
         }
-        numberArticlesCartIcon.textContent = nbTeddiesInCart;
     }
+    numberArticlesCartIcon.textContent = nbTeddiesInCart.toString();
 };
 
 // To execute displayNumberArticlesCartIcon() when DOM content is loaded (because it uses DOM elements)
